87ebc41f973fab54d41e054fe72d8d44bf015da9
cito125 <cito125@gmail.com>
2017-06-24 18:10:51 -0400

    Update network services to have rxJavaConverter factory

--- Ignite/app/src/main/java/hiphophacktivist/ignite/network/NetworkServices.java
+++ Ignite/app/src/main/java/hiphophacktivist/ignite/network/NetworkServices.java
@@ -1,6 +1,7 @@
 package hiphophacktivist.ignite.network;
 
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 
@@ -10,6 +11,7 @@ public class NetworkServices {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(base_url)
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
         return retrofit.create(networkServiceInterface);
     }
