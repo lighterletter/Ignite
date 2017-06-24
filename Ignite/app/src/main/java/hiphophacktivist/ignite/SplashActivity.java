9a7ea4bb9db57e8b51dd6aca8ba936654443707d
lighterletter <jgomez818@gmail.com>
2017-06-24 17:40:50 -0400

    adding logo

--- Ignite/app/src/main/java/hiphophacktivist/ignite/SplashActivity.java
+++ Ignite/app/src/main/java/hiphophacktivist/ignite/SplashActivity.java
@@ -2,6 +2,7 @@ package hiphophacktivist.ignite;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,8 +11,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 100ms
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
+
     }
 }
