5202281102533546a33ad4ed751345ece6664678
lighterletter <jgomez818@gmail.com>
2017-06-24 19:41:21 -0400

    adding styling

--- Ignite/app/src/main/java/hiphophacktivist/ignite/view/ProgramListingViewHolder.java
+++ Ignite/app/src/main/java/hiphophacktivist/ignite/view/ProgramListingViewHolder.java
@@ -1,9 +1,14 @@
 package hiphophacktivist.ignite.view;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 import hiphophacktivist.ignite.Model.CourseListing;
 import hiphophacktivist.ignite.R;
@@ -13,7 +18,7 @@ class ProgramListingViewHolder extends RecyclerView.ViewHolder {
         super(itemView);
     }
 
-    public void bind(CourseListing courseListing) {
+    public void bind(final CourseListing courseListing) {
         ((TextView)itemView.findViewById(R.id.listing)).setText(courseListing.getCourseName());
         ((TextView) itemView.findViewById(R.id.listing_borough)).setText(courseListing.getBorough());
         itemView.setOnClickListener(new View.OnClickListener() {
