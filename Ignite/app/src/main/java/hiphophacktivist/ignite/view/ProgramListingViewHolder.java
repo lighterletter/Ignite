7b119edeee56e7bc6eb86544a6bae2dea5827b1a
cito125 <cito125@gmail.com>
2017-06-24 19:38:09 -0400

    added third screen

--- Ignite/app/src/main/java/hiphophacktivist/ignite/view/ProgramListingViewHolder.java
+++ Ignite/app/src/main/java/hiphophacktivist/ignite/view/ProgramListingViewHolder.java
@@ -1,10 +1,12 @@
 package hiphophacktivist.ignite.view;
 
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import hiphophacktivist.ignite.DetailsActivity;
 import hiphophacktivist.ignite.Model.CourseListing;
 import hiphophacktivist.ignite.R;
 
@@ -13,13 +15,19 @@ class ProgramListingViewHolder extends RecyclerView.ViewHolder {
         super(itemView);
     }
 
-    public void bind(CourseListing courseListing) {
+    public void bind(final CourseListing courseListing) {
         ((TextView)itemView.findViewById(R.id.listing)).setText(courseListing.getCourseName());
         ((TextView) itemView.findViewById(R.id.listing_borough)).setText(courseListing.getBorough());
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(itemView.getContext(), DetailsActivity.class);
+                intent.putExtra(DetailsActivity.DESCRIPTION, courseListing.getCourseDescription());
+                intent.putExtra(DetailsActivity.ADDRESS, courseListing.getAddress());
+                intent.putExtra(DetailsActivity.PHONE_NUMBER, courseListing.getPhoneNumber());
+                intent.putExtra(DetailsActivity.WEBSITE,courseListing.getWebsite());
+                intent.putExtra(DetailsActivity.DURATION, courseListing.getDuration());
+                itemView.getContext().startActivity(intent);
             }
         });
     }
